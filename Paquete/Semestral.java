02c9a8d22b7e9333b41321e7e55ef42a55ede828
Rodolfo Campos <EngCamposRodolfo@gmail.com>
2023-07-23 01:54:06 -0500

    Create Semestral.java

---
+++ Paquete/Semestral.java
@@ -0,0 +1,11 @@
+package Paquete;
+
+public class Semestral {
+    private static String ruta;
+
+    public static String getRuta() {
+        ruta = "Paquete/Database.xlsx";
+        return ruta;
+    }
+
+}
