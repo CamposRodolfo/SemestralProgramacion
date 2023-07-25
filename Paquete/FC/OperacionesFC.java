c3a6adc54a7a5a99e399b6eaf85d1ed1ad748db8
Allan Vega <123776810+vega-allan-11@users.noreply.github.com>
2023-07-25 00:14:40 -0500

    Se añadió los elementos del enum OperacionesFC

--- Paquete/FC/OperacionesFC.java
+++ Paquete/FC/OperacionesFC.java
@@ -4,12 +4,18 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 import Paquete.Interfaces.Operacion;
-import Paquete.MV.IndiceEndeudamiento;
-import Paquete.MV.IndiceLiquidez;
 
 public enum OperacionesFC {
-  INDICE_DE_ENDEUDAMIENTO(IndiceEndeudamiento.class.getName()),
-  INDICE_DE_LIQUIDEZ(IndiceLiquidez.class.getName());
+  INDICE_DE_PAGO_A_PRINCIPAL(IndicePagoAPrincipal.class.getName()),
+  INDICADOR_DE_ROI(IndicadorRoi.class.getName()),
+  PROMEDIO_DE_RESPUESTA_A_CONSULTAS_DE_CLIENTES(PromedioRespuestaConsulta.class.getName()),
+  INDICE_DE_TENDENCIA(IndiceTendencia.class.getName()),
+  FUNCION_MEDIA_GEOMETRICA(FuncionMediaGeometrica.class.getName()),
+  INDICADOR_DE_TASA_DE_RETENCION_DE_CLIENTES(IndicadorTasaRetencionClientes.class.getName()),
+  INDICE_DE_PAGO(IndicePago.class.getName()),
+  CALCULO_DEL_VALOR_DE_UN_ACTIVO(CalculoValorActivo.class.getName()),
+  INDICADOR_DE_CRECIMIENTO__DE_VENTAS(IndicadorCrecimientoVentas.class.getName()),
+  RENTABILIDAD_DEL_CAPITAL_PROPIO(RentabilidadCapitalPropio.class.getName());
 
   private String claseOperacion;
 
